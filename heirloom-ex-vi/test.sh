faff8518e0af30d60b3cef8e13b895c35e8ede77
dHo <matt.donahoe@gmail.com>
2025-11-28 05:40:15 +0000

    term

--- heirloom-ex-vi/test.sh
+++ heirloom-ex-vi/test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 ./configure && make
-/root/dim/testty.py --run "./ex ex.h" --input "1,20p[enter]q[enter]" > /tmp/ex_test.txt
+TERM=xterm /root/dim/testty.py --run "./ex ex.h" --input "1,20p[enter]q[enter]" > /tmp/ex_test.txt
 diff test.txt /tmp/ex_test.txt && echo "pass"
-
