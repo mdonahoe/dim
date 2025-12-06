9a3e9fb211933b71c74b4c0ccd5951564105bb8e
dHo <matt.donahoe@gmail.com>
2025-12-06 04:13:00 +0000

    remove heirloom

--- heirloom-ex-vi/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-./configure && make
-TERM=xterm /root/dim/testty.py --run "./ex ex.h" --input "1,20p[enter]q[enter]" > /tmp/ex_test.txt
-diff test.txt /tmp/ex_test.txt && echo "pass"
